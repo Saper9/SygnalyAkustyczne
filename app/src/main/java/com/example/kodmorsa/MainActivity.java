73517732d8e1faeb15c22b4d76a4242994a96812
Saper9 <michalokon@wp.pl>
2021-05-17 22:39:20 +0200

    First

--- app/src/main/java/com/example/kodmorsa/MainActivity.java
+++ app/src/main/java/com/example/kodmorsa/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //co ten guzik robi
+
             }
         });
     }
