45455c72f2c5678d3028ee0190ccc524995a61bc
Saper9 <michalokon@wp.pl>
2021-06-01 18:03:07 +0200

    Dodana możliwość odtwarzania sygnału morse'a dla wpisanej frazy

--- app/src/main/java/com/example/kodmorsa/MainActivity.java
+++ app/src/main/java/com/example/kodmorsa/MainActivity.java
@@ -2,25 +2,65 @@ package com.example.kodmorsa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private Button recordButton;
+    private static Context context;
+    public static android.content.res.Resources res;
+    static int patternLength = 10;
+    static boolean isThreadWorking = false;
+    EditText morseTextField;
+    String pattern;
+    String morseSheetString="abcdefghijklmnoprstuvwxyz0123456789.,?!+-/ ";
+    private void PlayGeneratedPattern() {
+        Log.i("Pattern: ",pattern);
+        MorseTest.SetUpEverything();
+        MorseTest.GenerateSoundWave();
+        MorseTest.GenerateSoundWaveLine();
+        pattern=pattern.toUpperCase();
+        String morsePat = MorseTest.ConvertPatternToMorsePattern(pattern);
+        MorseTest.PlayPattern(morsePat);
 
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MainActivity.context = getApplicationContext();
         setContentView(R.layout.activity_main);
-
+        res = getResources();
         recordButton=findViewById(R.id.recordButton);
+        morseTextField=findViewById(R.id.textToMorseTextField);
         recordButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                //co ten guzik robi
-
+                if (!isThreadWorking) {
+                    //pattern = GeneratePattern(patternLength);
+                    pattern=morseTextField.getText().toString();
+                    isThreadWorking = true;
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            PlayGeneratedPattern();
+                        }
+                    }).start();
+                } else isThreadWorking = false;
             }
         });
     }
+    public static Context getAppContext() {
+        return MainActivity.context;
+    }
+
 }
